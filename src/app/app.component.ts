b2f8bc04b6acd5206579d14b80d38e7dd13c069b
Pavel Yudin <pavelyudin297@gmail.com>
2021-02-26 13:35:37 +0300

    AAAA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from './assets/services/api.service';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'custom-gitlab';
+
+  /** @internal */
+  public hello$: Observable<any>;
+
+  constructor(
+    private apiService: ApiService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.hello$ = this.apiService.helloWorld().pipe(map((item) => {
+      console.log(item);
+
+      return item;
+    }));
+  }
+
 }
